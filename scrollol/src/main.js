8291a066187bbb47ac30863c6887d99a14d3acb8
StefanLB <s_baliev@yahoo.com>
2020-03-27 00:00:28 +0200

    Added vuefire via "vue add vuefire"

--- scrollol/src/main.js
+++ scrollol/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import { firestorePlugin } from 'vuefire'
+
+Vue.use(firestorePlugin)
 
 Vue.config.productionTip = false
 
