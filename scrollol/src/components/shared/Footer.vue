63f421f71a38236d368d6d1ad4970917ccc04cf0
StefanLB <s_baliev@yahoo.com>
2020-03-27 23:49:53 +0200

    Removed unnecessary info from Home.vue, updated Footer.vue.

--- scrollol/src/components/shared/Footer.vue
+++ scrollol/src/components/shared/Footer.vue
@@ -1,46 +1,50 @@
 <template>
-  <v-footer
-    dark
-    padless
-  >
-    <v-card
-      class="flex"
-      flat
-      tile
-    >
-      <v-card-title class="teal">
-        <strong class="subheading">Get connected with us on social networks!</strong>
-
-        <v-spacer></v-spacer>
-
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4"
-          dark
-          icon
-        >
-          <v-icon size="24px">{{ icon }}</v-icon>
+  <v-footer dark padless>
+    <v-card class="flex" flat tile justify-content: center>
+      <v-card-title class="teal soc-icon">
+        <v-btn class="mx-4" dark icon>
+          <a href="https://www.facebook.com/">
+            <v-icon size="32px">fab fa-facebook</v-icon>
+          </a>
+        </v-btn>
+        <v-btn class="mx-4" dark icon>
+          <a href="https://www.twitter.com/">
+            <v-icon size="32px">fab fa-twitter</v-icon>
+          </a>
+        </v-btn>
+        <v-btn class="mx-4" dark icon>
+          <a href="https://github.com/StefanLB/">
+            <v-icon size="32px">fab fa-github</v-icon>
+          </a>
+        </v-btn>
+        <v-btn class="mx-4" dark icon>
+          <a href="https://www.linkedin.com/">
+            <v-icon size="32px">fab fa-linkedin</v-icon>
+          </a>
+        </v-btn>
+        <v-btn class="mx-4" dark icon>
+          <a href="https://www.instagram.com/">
+            <v-icon size="32px">fab fa-instagram</v-icon>
+          </a>
         </v-btn>
       </v-card-title>
 
-      <v-card-text class="py-2 white--text text-center">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-card-text>
+      <v-card-text
+        class="py-2 white--text text-center"
+      >© {{ new Date().getFullYear() }} Copyright: SoftUni Front-End Frameworks - Vue.js</v-card-text>
     </v-card>
   </v-footer>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      icons: [
-        'fab fa-facebook',
-        'fab fa-twitter',
-        'fab fa-google-plus',
-        'fab fa-linkedin',
-        'fab fa-instagram',
-      ],
-    }),
-  }
+export default {};
 </script>
+
+<style scoped>
+.soc-icon {
+  justify-content: center;
+}
+a {
+  color: whitesmoke;
+}
+</style>
