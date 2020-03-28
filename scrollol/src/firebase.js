de12181926e6db57094e7a5917cb9940affc767d
StefanLB <s_baliev@yahoo.com>
2020-03-28 19:49:19 +0200

    Added a few routes and authGuard. Changed favicon.ico

--- scrollol/src/firebase.js
+++ scrollol/src/firebase.js
@@ -1,38 +1,62 @@
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAKZVxFy3M01CxvFfOq3cNeXlj_rxosYM0",
-    authDomain: "scrollol.firebaseapp.com",
-    databaseURL: "https://scrollol.firebaseio.com",
-    projectId: "scrollol",
-    storageBucket: "scrollol.appspot.com",
-    messagingSenderId: "57334373708",
-    appId: "1:57334373708:web:dcc00ef2971549d7ba03e9"
-  };
+  apiKey: "AIzaSyAKZVxFy3M01CxvFfOq3cNeXlj_rxosYM0",
+  authDomain: "scrollol.firebaseapp.com",
+  databaseURL: "https://scrollol.firebaseio.com",
+  projectId: "scrollol",
+  storageBucket: "scrollol.appspot.com",
+  messagingSenderId: "57334373708",
+  appId: "1:57334373708:web:dcc00ef2971549d7ba03e9"
+};
 
 firebase.initializeApp(firebaseConfig);
 
+const db = firebase.firestore();
+const auth = firebase.auth();
+const currentUser = auth.currentUser;
+
+const settings = {
+  timestampsInSnapshots: true
+};
+db.settings(settings);
+
+const usersCollection = db.collection("users");
+const lolsCollection = db.collection("lols");
+const commentsCollection = db.collection("comments");
+
 export default {
-    auth: firebase.auth(),
-    loginGoogle() {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider)
+  db,
+  auth,
+  currentUser,
+  usersCollection,
+  lolsCollection,
+  commentsCollection,
+  loginGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
       .then(function(result) {
         console.log(result);
       })
-      .catch(function(error){
+      .catch(function(error) {
         const errorCode = error.code;
         const errorMessage = error.message;
         const email = error.email;
         const credential = error.credential;
         console.log(errorCode, errorMessage, email, credential);
-        })
-    },
-    logout() {
-      firebase.auth().signOut()
+      });
+  },
+  logout() {
+    firebase
+      .auth()
+      .signOut()
       .then(function() {})
       .catch(function(error) {
-        console.log(error)});
-    }
-}
+        console.log(error);
+      });
+  }
+};
--- scrollol/src/router/index.js
+++ scrollol/src/router/index.js
@@ -1,39 +1,70 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../components/Home.vue'
-import Register from '../components/authentication/Register.vue'
-import Login from '../components/authentication/Login.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import firebase from 'firebase';
 
+import Home from "../components/Home.vue";
+import About from "../components/About.vue";
+import Contact from "../components/Contact.vue";
+import Login from "../components/authentication/Login.vue";
+import Register from "../components/authentication/Register.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "*",
+    redirect: "/Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('../components/About.vue')
+    path: "/",
+    name: "Home",
+    component: Home
   },
   {
-    path: '/login',
-    name: 'Login',
+    path: "/about",
+    name: "About",
+    component: About
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact
+  },
+  {
+    path: "/login",
+    name: "Login",
     component: Login
   },
   {
-      path: '/register',
-      name: 'Register',
-      component: Register
+    path: "/register",
+    name: "Register",
+    component: Register
   }
-]
+];
+
+// WHEN YOU REACH A PATH WHICH REQUIRES AUTHENTICATION, ADD THE BELOW META
+// meta: {
+//   requiresAuth: true
+// }
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
+  const currentUser = firebase.auth().currentUser;
+
+  if (requiresAuth && !currentUser) {
+    next("/login");
+  } else if (requiresAuth && currentUser) {
+    next();
+  } else {
+    next();
+  }
+});
+
+export default router;
